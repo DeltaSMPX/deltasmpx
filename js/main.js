0eeb53e7d4269d6f5a8be8335486b0bf9af90720
DeltaSMPX <moonlightisxd@gmail.com>
2022-04-05 17:19:09 +0300

    New FIles

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function(_0xe05bd1, _0x46e38b) {
+    const _0x1f1e2a = _0x302c,
+        _0x230d93 = _0xe05bd1();
+    while (!![]) {
+        try {
+            const _0x2f6cdb = -parseInt(_0x1f1e2a(0xc6)) / 0x1 + -parseInt(_0x1f1e2a(0xcb)) / 0x2 + parseInt(_0x1f1e2a(0xc7)) / 0x3 * (-parseInt(_0x1f1e2a(0xbd)) / 0x4) + -parseInt(_0x1f1e2a(0xc3)) / 0x5 * (parseInt(_0x1f1e2a(0xba)) / 0x6) + -parseInt(_0x1f1e2a(0xbc)) / 0x7 + parseInt(_0x1f1e2a(0xd1)) / 0x8 * (parseInt(_0x1f1e2a(0xb3)) / 0x9) + -parseInt(_0x1f1e2a(0xb4)) / 0xa * (-parseInt(_0x1f1e2a(0xc0)) / 0xb);
+            if (_0x2f6cdb === _0x46e38b) break;
+            else _0x230d93['push'](_0x230d93['shift']());
+        } catch (_0x1c102f) { _0x230d93['push'](_0x230d93['shift']()); }
+    }
+}(_0x5104, 0x8594c));
+
+function _0x302c(_0x511317, _0x4e40fc) { const _0x510469 = _0x5104(); return _0x302c = function(_0x302cf3, _0x5b5512) { _0x302cf3 = _0x302cf3 - 0xb2; let _0x664fe9 = _0x510469[_0x302cf3]; return _0x664fe9; }, _0x302c(_0x511317, _0x4e40fc); }
+
+function _0x5104() {
+    const _0x44fe13 = ['lname', '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20**How\x20many\x20hours\x20are\x20you\x20active\x20a\x20day?**\x20', '12epNtUR', 'email', '934570DLjOVy', '15100tHqYaW', '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20**Do\x20you\x20use\x20a\x20mic?**\x20', 'https://discord.com/api/webhooks/938903995819376680/FlJt8IqF0D5lwt_RaEDI6mDjcLSvBVKOIN79hejw9CxusZRdhASuT5u6L0-TMVsOHAYh', '154nCmxbq', 'Massage\x20Sent!', 'Whitelist\x20Request', '1786580bazsEn', 'steam', 'getElementById', '112046cRvPzK', '654xouxae', 'fname', '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20**Are\x20you\x20streaming/want\x20to\x20start?**\x20', 'value', '749442PzmAVN', 'The\x20Form\x20Is\x20Submitted.', 'stringify', 'why', 'then', 'log', '8dkuNjS', 'json', '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20**Are\x20you\x20fine\x20with\x20being\x20on\x20stream?**\x20', '132777PoFRev', '1921610MVeCIW', 'dis', '?wait=true', '**Discord\x20Name:**\x20'];
+    _0x5104 = function() { return _0x44fe13; };
+    return _0x5104();
+}
+
+function webhookSend() {
+    const _0x5cf9bc = _0x302c,
+        _0x2a7e69 = _0x5cf9bc(0xbf);
+    let _0x398b78 = document[_0x5cf9bc(0xc5)](_0x5cf9bc(0xc8))['value'],
+        _0x639901 = document[_0x5cf9bc(0xc5)](_0x5cf9bc(0xb8))[_0x5cf9bc(0xca)],
+        _0x1439dc = document[_0x5cf9bc(0xc5)](_0x5cf9bc(0xb5))[_0x5cf9bc(0xca)],
+        _0x286e83 = document[_0x5cf9bc(0xc5)](_0x5cf9bc(0xbb))[_0x5cf9bc(0xca)],
+        _0x49d882 = document[_0x5cf9bc(0xc5)](_0x5cf9bc(0xc4))['value'],
+        _0x1417a2 = document[_0x5cf9bc(0xc5)]('date')[_0x5cf9bc(0xca)],
+        _0x147838 = document[_0x5cf9bc(0xc5)](_0x5cf9bc(0xce))[_0x5cf9bc(0xca)];
+    const _0xdac734 = { 'embeds': [{ 'title': _0x5cf9bc(0xc2), 'color': 0x0, 'description': _0x5cf9bc(0xb7) + _0x398b78 + '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20**Twitch\x20Name:**\x20' + _0x639901 + _0x5cf9bc(0xb2) + _0x1439dc + _0x5cf9bc(0xbe) + _0x286e83 + '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20**Are\x20you\x20fluent\x20in\x20English?**\x20' + _0x49d882 + _0x5cf9bc(0xb9) + _0x1417a2 + _0x5cf9bc(0xc9) + _0x147838, 'footer': { 'text': 'Whitelist\x20Request\x20By\x20' + _0x398b78 } }] };
+    (_0x398b78, _0x639901, _0x1439dc, _0x286e83, _0x49d882, _0x1417a2, _0x147838 == '') ? alert('You\x20missed\x20something\x20there'): (fetch(_0x2a7e69 + _0x5cf9bc(0xb6), { 'method': 'POST', 'headers': { 'content-type': 'application/json' }, 'body': JSON[_0x5cf9bc(0xcd)](_0xdac734) })[_0x5cf9bc(0xcf)](_0x2da6d7 => _0x2da6d7[_0x5cf9bc(0xd2)]()), alert(_0x5cf9bc(0xcc)), console[_0x5cf9bc(0xd0)](_0x5cf9bc(0xc1)));
+}
